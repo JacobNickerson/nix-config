7a270b5e63ae4d645541beeb3fc9d86c185adf91
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2025-12-30 04:03:27 -0500

    Add nvim config

--- modules/home/jacobnickerson.nix
+++ modules/home/jacobnickerson.nix
@@ -246,7 +246,17 @@
 
     neovim = {
       enable = true;
+
+      viAlias = true;
+      vimAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        pkgs.vimPlugins.nightfly
+        pkgs.vimPlugins.nvim-treesitter
+        pkgs.vimPlugins.lualine-nvim
+      ];
       extraLuaConfig = ''
+        vim.opt.termguicolors = true
+        vim.cmd [[colorscheme nightfly]]
         vim.opt.tabstop = 2
         vim.opt.shiftwidth = 2
         vim.opt.softtabstop = 2

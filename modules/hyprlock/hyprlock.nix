cddad57e57e66b35438a8c185b77dc7b4c3bc652
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-01-03 01:04:06 -0500

    Hyprlock updates
    Added profile pic and background, included additional packages required
    for weather and uptime. Fixed weather script to avoid querying for
    coordinates (API requests were blocked due to not using an HTTP-agent
    and not being cached)

--- modules/hyprlock/hyprlock.nix
+++ modules/hyprlock/hyprlock.nix
@@ -5,13 +5,13 @@
 {
   home.packages = with pkgs; [
     pkgs.nerd-fonts.caskaydia-cove
+    pkgs.jq
+    pkgs.procps
   ];
   home.file = {
-    ".config/hypr/hyprlock.conf" = {
-      source = ./hyprlock.conf;
-    };
-    ".config/hypr/macchiato.conf" = {
-      source = ./macchiato.conf;
-    };
+    ".config/hypr/hyprlock.conf".source = ./hyprlock.conf;
+    ".config/hypr/macchiato.conf".source = ./macchiato.conf; 
+    ".config/hypr/hyprlock-bg.png".source = ./lake.png;
+    ".config/hypr/profile.jpg".source = ./profile.jpg;
   };
 }

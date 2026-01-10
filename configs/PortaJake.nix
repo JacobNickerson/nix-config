c92dfb35bcecce89188e4941d6d1d33aa485ad7e
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-01-09 22:53:06 -0500

    Set some power-saving settings

--- configs/PortaJake.nix
+++ configs/PortaJake.nix
@@ -1,5 +1,17 @@
+{ lib, ... }:
 {
   imports = [
     ./common.nix
   ];
+  # Battery life settings
+  services.power-profiles-daemon.enable = true;
+  powerManagement.powertop.enable = true;
+  boot.kernelParams = [
+    "pcie_aspm=force"  # May cause instability, remove if so
+    "amdgpu.runpm=1"
+    "amdpstate=active"
+  ];  
+  networking.wireless.enable = lib.mkForce false;
+  networking.wireless.iwd.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
 }

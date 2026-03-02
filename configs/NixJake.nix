6df5279399f15bb44c7d71d0e12f0978e96cbe81
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-03-02 03:50:05 -0500

    Add udev rule for QMK flashing

--- configs/NixJake.nix
+++ configs/NixJake.nix
@@ -18,4 +18,8 @@
       boost = pkgs.boost187; # NOTE: Boost update broke a lot of things, this will eventually be fixed but for now pin this package
     };
   };
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTR{idVendor}=="1eaf", ATTR{idProduct}=="0003", MODE="0666"
+  '';
 }

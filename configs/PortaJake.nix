9843ce14ececeeedad30bce046321ece826fb716
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-03-05 13:03:47 -0500

    Add HDMI audio settings (fr this time)

--- configs/PortaJake.nix
+++ configs/PortaJake.nix
@@ -30,17 +30,17 @@
   boot.initrd.systemd.enable = true;
 
   # HDMI Audio
-  # services.pipewire.wireplumber.extraConfig."51-alsa-auto-switch" = {
-  #   "monitor.alsa.rules" = [
-  #     {
-  #       matches = [
-  #         { "device.name" = "~alsa_card.*"; }
-  #       ];
-  #       actions.update-props = {
-  #         "api.acp.auto-profile" = true;
-  #         "api.acp.auto-port" = true;
-  #       };
-  #     }
-  #   ];
-  # };
+  services.pipewire.wireplumber.extraConfig."51-alsa-auto-switch" = {
+    "monitor.alsa.rules" = [
+      {
+        matches = [
+          { "device.name" = "~alsa_card.*"; }
+        ];
+        actions.update-props = {
+          "api.acp.auto-profile" = true;
+          "api.acp.auto-port" = true;
+        };
+      }
+    ];
+  };
 }

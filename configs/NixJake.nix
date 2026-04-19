b6251d972cd46f81040c7a9541d94c5a7b71356f
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-04-19 03:42:24 -0400

    Remove some unnecessary settings

--- configs/NixJake.nix
+++ configs/NixJake.nix
@@ -19,12 +19,4 @@
       cudaSupport = true;
     };
   };
-
-  services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTR{idVendor}=="1eaf", ATTR{idProduct}=="0003", MODE="0666"
-  '';
-
-  services.logind.settings.Login = {
-    InhibitDelayMaxSec = 1;
-  };
 }

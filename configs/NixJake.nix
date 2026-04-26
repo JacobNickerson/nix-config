54bd46105f17b1f51077ff40c1dc58f81449cde7
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-04-26 17:24:17 -0400

    Use new sunshine and nix-helper modules

--- configs/NixJake.nix
+++ configs/NixJake.nix
@@ -4,19 +4,11 @@
     ./common.nix
     ../modules/openssh/NixJake.nix
     ../modules/virt-manager.nix
+    (import ../modules/nix-helper.nix { flake_path = "/home/jacobnickerson/nix-config"; })
+    (import ../modules/sunshine.nix { pkgs = pkgs; use_cuda = true; })
   ];
 
   environment.systemPackages = with pkgs; [
     ntfs3g
   ];
-  
-  services.sunshine = {
-    enable = true;
-    autoStart = false;
-    capSysAdmin = true;
-    openFirewall = true;
-    package = pkgs.sunshine.override {
-      cudaSupport = true;
-    };
-  };
 }

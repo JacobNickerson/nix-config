362b71b8066e8a748f27489cfbc5705d3307626e
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-05-10 01:08:43 -0400

    Remove unnecessary settings from fish config

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -4,9 +4,7 @@
 		enable = true;
 
 		shellInit = ''
-			# Universal variables
 			set -U MANROFFOPT "-c"
-			set -U MANPAGER "sh -c 'col -bx | bat -l man -p'"
 			set -U __done_min_cmd_duration 10000
 			set -U __done_notification_urgency_level low
 		'';
@@ -14,7 +12,6 @@
 		interactiveShellInit = ''
 			fish_add_path ~/.local/bin ~/.cargo/bin ~/.dotnet/tools
 
-			# Bindings for !! and !$
 			if [ "$fish_key_bindings" = fish_vi_key_bindings ]
 				bind -Minsert ! __history_previous_command
 				bind -Minsert '$' __history_previous_command_arguments
@@ -36,19 +33,6 @@
 			'';
 			history = ''
 				builtin history --show-time='%F %T '
-			'';
-			backup = ''
-				cp $argv[1] $argv[1].bak
-			'';
-			copy = ''
-				set count (count $argv | tr -d \n)
-				if test "$count" = 2; and test -d "$argv[1]"
-						set from (echo $argv[1] | trim-right /)
-						set to (echo $argv[2])
-						command cp -r $from $to
-				else
-						command cp $argv
-				end
 			'';
 			__history_previous_command = ''
 				switch (commandline -t)

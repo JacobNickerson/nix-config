1e16599b90a70413021b317a49c36230ba3a8a1e
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-05-16 20:30:01 -0400

    Add NAS to NixJake

--- configs/NixJake.nix
+++ configs/NixJake.nix
@@ -2,6 +2,9 @@
 {
   imports = [
     ./common.nix
+    (import ../modules/nas.nix {
+      use_vpn = false;
+    })
     ../modules/openssh/NixJake.nix
     ../modules/virt-manager.nix
     (import ../modules/sunshine.nix { pkgs = pkgs; use_cuda = true; })

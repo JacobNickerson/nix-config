306b46f084cfd2bb16a367bd8970f6a265fa2612
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-06-09 00:38:03 -0400

    Update ripgrep config

--- users/modules/cli-tools.nix
+++ users/modules/cli-tools.nix
@@ -10,7 +10,7 @@ in
 	config = lib.mkIf cfg.enable {
 		programs.ripgrep = {
 			enable = true;
-			arguments = [ ];
+			arguments = [ "--smart-case" ];
 		};
 		programs.ripgrep-all = {
 			enable = true;

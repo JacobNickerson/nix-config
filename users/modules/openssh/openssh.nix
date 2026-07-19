64a1baef45658b97e70cc5c6e0636269248b2306
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-07-19 12:42:17 -0400

    Fix casing and add an entry for vscode

--- users/modules/openssh/openssh.nix
+++ users/modules/openssh/openssh.nix
@@ -36,17 +36,26 @@ in
       enableDefaultConfig = false;
       settings = extra_entries // {
         "github.com" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = [ "~/.ssh/github" ];
-          identitiesOnly = true;
+          HostName = "github.com";
+          User = "git";
+          IdentityFile = [ "~/.ssh/github" ];
+          IdentitiesOnly = true;
         };
         "gubbserver" = {
-          hostname = "192.168.5.33";
-          port = 42067;
-          user = "jacobnickerson";
-          identityFile = [ "~/.ssh/gubbserver" ];
-          identitiesOnly = true;
+          HostName = "192.168.5.33";
+          Port = 42067;
+          User = "jacobnickerson";
+          IdentityFile = [ "~/.ssh/gubbserver" ];
+          IdentitiesOnly = true;
+        };
+        "gubbserver-code" = {
+          HostName = "192.168.5.33";
+          Port = 42067;
+          User = "jacobnickerson";
+          IdentityFile = [ "~/.ssh/gubbserver" ];
+          IdentitiesOnly = true;
+          RequestTTY = "force";
+          RemoteCommand = "/usr/bin/env bash";
         };
       };
     };

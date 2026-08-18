2527e7319fdc0e2077bee43a61380b08cb38a172
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-08-18 18:56:48 -0400

    Create an mcp-searxng derivation

--- users/modules/mcp.nix
+++ users/modules/mcp.nix
@@ -2,6 +2,19 @@
 
 let
 	cfg = config.myUserModules.mcp;
+
+	version = "1.15.0";
+	mcp-searxng = pkgs.buildNpmPackage {
+		pname = "mcp-searxng";
+		version = version;
+		src = pkgs.fetchFromGitHub {
+			owner = "ihor-sokoliuk";
+			repo = "mcp-searxng";
+			rev = "v${version}";
+			sha256 = "sha256-vNh96VKUD+hrwVhHvVAk88IKunfsY2gQ8WWmgRaywqY=";
+		};
+		npmDepsHash = "sha256-LYVlgiEIZ2MoLifrASjQxeZjqKfKccKZKMFFfSR6kUs=";
+	};
 in
 {
 	options.myUserModules.mcp = {
@@ -20,8 +33,9 @@ in
 		programs.mcp.enable = true;
 		programs.mcp.servers = {
 			searxng = lib.mkIf cfg.searxng.enable {
-				command = "${pkgs.nodejs_22}/bin/npx";
-				args = [ "-y" "mcp-searxng@latest" ];
+				type = "local";
+				command = "${mcp-searxng}/bin/mcp-searxng";
+				args = [];
 				env = {
 					SEARXNG_URL = "https://searxng.knitnet.org";
 				};
